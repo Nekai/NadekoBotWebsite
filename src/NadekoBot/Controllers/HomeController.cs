dd01ea1f0df939cc45399aa235071dbd0fc6a051
Kwoth <srki0509@gmail.com>
2016-07-30 21:16:37 +0200

    Now showing server count from carbonitex

--- src/NadekoBot/Controllers/HomeController.cs
+++ src/NadekoBot/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace NadekoBot.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewData["server_count"] = await GetServerCount();
             return View();
+        }
+
+        private async Task<string> GetServerCount()
+        {
+            using (var http = new HttpClient()) {
+                var res = await http.GetAsync("https://www.carbonitex.net/discord/bots");
+                var str = await res.Content.ReadAsStringAsync();
+                var m = Regex.Match(str, @"test=\\""(?<count>\d*)\\"" name=\\""Nadeko\\""");
+                return m.Groups["count"].Value;
+            }
         }
 
         public IActionResult Error()
